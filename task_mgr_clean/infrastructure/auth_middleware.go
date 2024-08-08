9e81eb50003933405ce1a130f9601a95435319c7
dagota12 <chernetdagim@gmail.com>
2024-08-08 17:42:25 +0300

    added middleware and auth

--- task_mgr_clean/infrastructure/auth_middleware.go
+++ task_mgr_clean/infrastructure/auth_middleware.go
@@ -1,1 +1,40 @@
 package infrastructure
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Authorize(allowedRoles ...string) gin.HandlerFunc {
+
+	return func(ctx *gin.Context) {
+
+		authHeader := ctx.GetHeader("Authorization")
+		if authHeader == "" {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized: Missing token"})
+			ctx.Abort()
+			return
+		}
+		//Parse the JWT token
+		claim, err := ParseToken(authHeader)
+		if err != nil {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			ctx.Abort()
+			return
+		}
+		for _, role := range allowedRoles {
+			if claim.Role == role {
+				// log.Printf("CLAIM: user_id: %v, role: %v", claim["user_id"], claim["role"])
+				ctx.Set("claim", claim)
+				ctx.Next()
+				return
+			}
+		}
+
+		ctx.JSON(http.StatusForbidden, gin.H{"error": "Forbidden: Insufficient permissions"})
+		ctx.Abort()
+
+	}
+
+}
--- task_mgr_clean/infrastructure/jwt_service.go
+++ task_mgr_clean/infrastructure/jwt_service.go
@@ -1,1 +1,54 @@
 package infrastructure
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type Claims struct {
+	UserID string `json:"user_id"`
+	Role   string `json:"role"`
+	jwt.StandardClaims
+}
+
+func GenerateToken(userID string, role string) (string, error) {
+	claims := Claims{
+		UserID: userID,
+		Role:   role,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * time.Duration(EXP_TIME)).Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(JWT_SECRET))
+}
+
+func ParseToken(authHeader string) (Claims, error) {
+	authParts := strings.Split(authHeader, " ")
+
+	if len(authParts) != 2 || strings.ToLower(authParts[0]) != "bearer" {
+		return Claims{}, fmt.Errorf("invalid authorization header format")
+	}
+
+	token, err := jwt.Parse(authParts[1], func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method %v", token.Header["alg"])
+		}
+		return []byte(JWT_SECRET), nil
+	})
+	if err != nil {
+		return Claims{}, err
+	}
+	claims, ok := token.Claims.(Claims) //type assertion for Claims
+	fmt.Println("Claims: ", claims)
+	if !ok || !token.Valid {
+		return Claims{}, fmt.Errorf("invalid JWT token")
+	}
+
+	return claims, nil
+}
